4190c8358ebe11935ae3f094d5af1b3d333c468e
Oguzhan-bay <bayoguzhan95@gmail.com>
2023-08-12 01:12:33 +0300

    Custom TD

--- src/components/table/TableItem.jsx
+++ src/components/table/TableItem.jsx
@@ -28,27 +28,33 @@ export const TableItem = ({ data }) => {
       <TableItemHeaders firstItem={`${data.D}, ${data.DAY}, ${data.LN}`} />
 
       <tr className="text-sm">
-        <td className={`border w-1/5 p-2  `}>
+        <td className={`border min-w-[120px]  w-1/5 p-2  `}>
           <strong>{data.C}</strong> {`, ${data.T}, ${data.N}`}
         </td>
-        <td className={`custom-cell`}>Yorumlar</td>
-        <td className={`custom-cell`}>{data.OCG['1'].MBS}</td>
+        <TdItem data={data} isCustomCell={true}>
+          Yorumlar
+        </TdItem>
+        <TdItem data={data} isCustomCell={true}>
+          {data.OCG['1'].MBS}
+        </TdItem>
         <TdItem data={data} code={data.C} ocgKey="1" ocKey="0" clickHandler={clickHandler} />
         <TdItem data={data} code={data.C} ocgKey="1" ocKey="1" clickHandler={clickHandler} />
-        <td className={`custom-cell`}></td>
+        <TdItem data={data} isCustomCell={true}></TdItem>
         <TdItem data={data} code={data.C} ocgKey="5" ocKey="25" clickHandler={clickHandler} />
         <TdItem data={data} code={data.C} ocgKey="5" ocKey="26" clickHandler={clickHandler} />
-        <td className={`custom-cell `}></td>
-        <td className={`custom-cell `}></td>
-        <td className={`custom-cell `}></td>
-        <td className={`custom-cell `}></td>
-        <td className={`custom-cell `}></td>
+        <TdItem data={data} isCustomCell={true}></TdItem>
+        <TdItem data={data} isCustomCell={true}></TdItem>
+        <TdItem data={data} isCustomCell={true}></TdItem>
+        <TdItem data={data} isCustomCell={true}></TdItem>
+        <TdItem data={data} isCustomCell={true}></TdItem>
         <TdItem data={data} code={data.C} ocgKey="2" ocKey="3" clickHandler={clickHandler} />
         <TdItem data={data} code={data.C} ocgKey="2" ocKey="4" clickHandler={clickHandler} />
         <TdItem data={data} code={data.C} ocgKey="2" ocKey="5" clickHandler={clickHandler} />
-        <td className={`custom-cell`}></td>
-        <td className={`custom-cell`}></td>
-        <td className={`custom-cell`}>3</td>
+        <TdItem data={data} isCustomCell={true}></TdItem>
+        <TdItem data={data} isCustomCell={true}></TdItem>
+        <TdItem data={data} isCustomCell={true}>
+          3
+        </TdItem>
       </tr>
     </React.Fragment>
   );
--- src/components/table/TdItem.jsx
+++ src/components/table/TdItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export const TdItem = ({ data, code, ocgKey, ocKey, clickHandler }) => {
+export const TdItem = ({ data, code, ocgKey, ocKey, clickHandler, isCustomCell, children }) => {
+  if (isCustomCell) {
+    return <td className="custom-cell">{children}</td>;
+  }
+
   const value = data?.OCG[ocgKey]?.OC[ocKey]?.O;
   const className = React.useMemo(() => {
     const isSelected = clickHandler && clickHandler.isSelected(code, value);
-    return `border cursor-pointer hover:bg-gray-200 transition ease-linear duration-100 text-center p-2  ${isSelected ? 'bg-yellow-300' : ''}`;
+    return `border cursor-pointer hover:bg-gray-200 transition ease-linear duration-100 text-center p-2 ${
+      isSelected ? 'bg-yellow-300' : ''
+    }`;
   }, [clickHandler, code, value]);
 
   return (
